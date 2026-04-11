36e6789ac46b2a779ab35d8318ec0f47b8a1852b
Asermajeedsh <Aser.majeed.sh@gmail.com>
2026-04-11 19:51:19 +0500

    Fix middleware syntax errors and remove duplicates

--- middleware.ts
+++ middleware.ts
@@ -73,42 +73,6 @@ export async function middleware(request: NextRequest) {
           response.cookies.set({
             name,
             value: '',
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
             ...options,
           })
         },
